ca67925a93db5be7e05fc7c18092ed240586fc0a
Marcelo Origoni <marcelo@origoni.com.ar>
2010-07-05 23:00:37 -0300

    mooltiselect v1.1

--- mooltiselect.js
+++ mooltiselect.js
@@ -5,25 +5,30 @@
 			options: 'option',
 			selectedClass: 'selected',
 			name: 'listBox',
-			sort: false
+			sort: false,
+			maximum: 0
 		},
 		initialize: function(options) {
 			this.setOptions(options);
 		},
 		apply: function(properties){
 			
-			if(properties){
-				list = properties.list;
-				options = '.' + properties.options;
-				name = properties.name + "[]";
-				selected = properties.selectedClass;
-				sort = properties.sort;
-			}else{	
+			var list, sort, maximum, options, name, selected;
+			
+			if(properties){ 
+				list = properties.list ? properties.list : this.options.list;
+				sort = properties.sort ? properties.sort : this.options.sort;
+				maximum = properties.maximum ? properties.maximum : this.options.maximum;
+				options = '.' + properties.options ? properties.options : this.options.options;
+				name = properties.name ? properties.name + '[]': this.options.name + '[]';
+				selected = properties.selectedClass ? properties.selectedClass : this.options.selectedClass;
+			}else{
 				list = this.options.list;
-				options = '.' + this.options.options;
-				name = this.options.name + "[]";
-				selected = this.options.selectedClass;
 				sort = this.options.sort;
+				maximum = this.options.maximum;
+				options = '.' + this.options.options;
+				name = this.options.name + '[]';
+				selected = this.options.selectedClass;			
 			}
 			
 			if(sort){
@@ -33,18 +38,24 @@
 				$(list).getElements(options).each(function(el,i){
 					$(el).setProperty('name', name);
 					$(el).setProperty('sel', selected);
+					$(el).setProperty('max', maximum);
 					$(el).addEvent('dblclick', function(){
 						if($(el).hasClass($(el).getProperty('sel'))){
 							$(el).removeClass($(el).getProperty('sel'));
 							$(el).getFirst('input').destroy();
 						}else{
-							$(el).addClass($(el).getProperty('sel'));
-							$(el).adopt(new Element('input', {
-								'name': $(el).getProperty('name'),
-								'id': $(el).getProperty('name'),
-								'value': $(el).getProperty('rel'),
-								'type': 'hidden'
-								}));
+							var selItems = $(this).getParent().getChildren('.' + $(el).getProperty('sel')).length;
+							if(selItems < $(el).getProperty('max') && $(el).getProperty('max') > 0){
+								$(el).addClass($(el).getProperty('sel'));
+								$(el).adopt(new Element('input', {
+									'name': $(el).getProperty('name'),
+									'id': $(el).getProperty('name'),
+									'value': $(el).getProperty('rel'),
+									'type': 'hidden'
+									}));
+								}else{
+									alert('You already selected the maximum of ' + $(el).getProperty('max') + ' items');
+								}
 							}
 						});
 				 sortList.grab($(el));
@@ -58,18 +69,24 @@
 				$(list).getElements(options).each(function(el,i){
 					$(el).setProperty('name', name);
 					$(el).setProperty('sel', selected);
+					$(el).setProperty('max', maximum);					
 					$(el).addEvent('click', function(){
 						if($(el).hasClass($(el).getProperty('sel'))){
 							$(el).removeClass($(el).getProperty('sel'));
 							$(el).getFirst('input').destroy();
 						}else{
-							$(el).addClass($(el).getProperty('sel'));
-							$(el).adopt(new Element('input', {
-								'name': $(el).getProperty('name'),
-								'id': $(el).getProperty('name'),
-								'value': $(el).getProperty('rel'),
-								'type': 'hidden'
-								}));
+							var selItems = $(this).getParent().getChildren('.' + $(el).getProperty('sel')).length;
+							if(selItems < $(el).getProperty('max') && $(el).getProperty('max') > 0){						
+								$(el).addClass($(el).getProperty('sel'));
+								$(el).adopt(new Element('input', {
+									'name': $(el).getProperty('name'),
+									'id': $(el).getProperty('name'),
+									'value': $(el).getProperty('rel'),
+									'type': 'hidden'
+									}));
+								}else{
+									alert('You already selected the maximum of ' + $(el).getProperty('max') + ' items');
+								}
 							}
 						});
 				});
